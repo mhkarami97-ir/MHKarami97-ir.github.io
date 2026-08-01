c58b485600643ff5a06fa95d900c017aa8ed27b1
mhkarami97 <mhkarami97@gmail.com>
2026-08-01 20:16:03 +0330

    fix

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -296,7 +296,7 @@ const CONFIG = {
   },
   website: {
     travel: 'KaramTravel.ir',
-    tour: 'tour.mhkarami97.ir',
+    tour: 'Tour.mhkarami97.ir',
     experience: 'Zoorvan.ir',
     shop: 'ShirdalMarket.ir',
     house: 'YarKhoone.ir',
