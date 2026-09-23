585180672950fb7b4447cbba8db0ef6aeaaf24aa
mhkarami97 <mhkarami97@gmail.com>
2026-09-23 14:34:59 +0330

    fix

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -365,12 +365,12 @@ const CONFIG = {
       {
         name: 'New Tab',
         imageUrl: '/icons/extension/01.png',
-        link: 'https://microsoftedge.microsoft.com/addons/detail/%D8%AF%D8%A7%D8%B4%D8%A8%D9%88%D8%B1%D8%AF-%D9%81%D8%A7%D8%B1%D8%B3%DB%8C/mdnmgigpikijbhjenbjknhcnldebobpa',
+        link: 'https://microsoftedge.microsoft.com/addons/detail/my-persian-dashboard/mdnmgigpikijbhjenbjknhcnldebobpa',
       },
       {
         name: 'Font Changer',
         imageUrl: '/icons/extension/02.png',
-        link: 'https://microsoftedge.microsoft.com/addons/detail/font-changer-for-google-f/hhalocjgpdnjfoimnaaphihomijagcmo',
+        link: 'https://microsoftedge.microsoft.com/addons/detail/font-changer/hhalocjgpdnjfoimnaaphihomijagcmo',
       },
       {
         name: 'Easy Translate',
